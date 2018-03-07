415b18c5462908550ee7f3dba3431a909ad81071
jian玄冰 <jianxuanhuo1@126.com>
2018-03-07 16:26:57 +0800

    添加 创蓝253 短信库

---
+++ src/Cosmos.Business.Extensions.SMS.ChuangLan/Configuration/ChuangLanAccount.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Cosmos.Business.Extensions.SMS.Abstractions;
+
+namespace Cosmos.Business.Extensions.SMS.ChuangLan.Configuration
+{
+    public class ChuangLanAccount:IAccountSettings
+    {
+        public string User { get; set; }
+        public string Key { get; set; }
+
+        public string ApiUrl { get; set; }
+    }
+}
---
+++ src/Cosmos.Business.Extensions.SMS.ChuangLan/Configuration/ChuangLanConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Cosmos.Business.Extensions.SMS.Abstractions;
+
+namespace Cosmos.Business.Extensions.SMS.ChuangLan.Configuration
+{
+    public class ChuangLanConfig:IConfig
+    {
+        public ChuangLanAccount CodeAccount { get; set; }
+
+        public ChuangLanAccount MarketingAccount { get; set; }
+
+        public int RetryTimes { get; set; } = 3;
+    }
+}
