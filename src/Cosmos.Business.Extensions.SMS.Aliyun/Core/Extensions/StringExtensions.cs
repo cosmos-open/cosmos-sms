c0a93dfb0bab92d0c61702b6475b1d46cad956bb
seayxu <seayxu@gmail.com>
2018-03-10 00:05:35 +0800

    Fixed 'SignatureDoesNotMatch' bug for aliyun.

---
+++ src/Cosmos.Business.Extensions.SMS.Aliyun/Core/Extensions/StringExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Cosmos.Business.Extensions.SMS.Aliyun.Core.Extensions
+{
+    public static class StringExtensions{
+        public static byte[] HexToBytes(this string value) {
+            int index = 0;
+            List<byte> list = new List<byte>(value.Length / 2);
+
+            while (index< value.Length)
+            {
+                index += 2;
+                list.Add(byte.Parse(value.Substring(index, 2), System.Globalization.NumberStyles.AllowHexSpecifier));
+            }
+
+            return list.ToArray();
+        }
+    }
+}
