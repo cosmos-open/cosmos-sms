b6ff095da1a9aed277521f53c65957eb01a5986f
alexinea <forerunner@outlook.com>
2019-09-28 16:29:35 +0800

    Code clean.

--- src/Cosmos.Business.Extensions.SMS.RunsOn.AspNetCore/AspNetCoreSmsSender.cs
+++ src/Cosmos.Business.Extensions.SMS.RunsOn.AspNetCore/AspNetCoreSmsSender.cs
@@ -1,8 +1,10 @@
 ﻿using System.Collections.Generic;
 using Cosmos.Business.Extensions.SMS.Core.Aggregation;
 
-namespace Cosmos.Business.Extensions.SMS.RunsOn.AspNetCore {
-    public class AspNetCoreSmsSender : SmsSenderBase {
+namespace Cosmos.Business.Extensions.SMS.RunsOn.AspNetCore
+{
+    public class AspNetCoreSmsSender : SmsSenderBase
+    {
         public AspNetCoreSmsSender(ISmsAggregationSender sender, SmsBizPkgConfiguration configuration, List<string> specificImplementList = null)
             : base(sender, configuration, specificImplementList) { }
     }
